c292a2bfbbcc1e25c513c689d71517aaff827499
Beverly M <bevponi@gmail.com>
2023-04-12 22:01:48 +0000

    empezando

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,22 +1,17 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import TodoList from "./TodoList";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="Container">
-			<h2>Mi Lista Todos</h2>
 			
-			<ul>
-				<li><input type="text" placeholder="Add More"></input></li>
-				<li>Tomar café</li>
-				<li>Hacer mi cama</li>				
-				<li>Hacer el proyecto</li>
-				<li>Flojear el resto del día</li>
+			<div>
+				<TodoList />
+			</div>
 
-			</ul>
 
 		</div>
 	);
