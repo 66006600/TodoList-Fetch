26509b62d49d8417af9eb7cd39575491fad97054
Beverly M <bevponi@gmail.com>
2023-04-13 21:42:26 +0000

    Applied fixes

--- src/js/component/TodoList.js
+++ src/js/component/TodoList.js
@@ -2,16 +2,11 @@ import React from "react";
 import TodoFilters from "./TodoFilters";
 
 const TodoList = ({ children }) => {
-    retun (
+    return (
         <div className="flex flex-col mt-7 rounded-lg overflow-hiden shadow-2xl">
             { children }
             <TodoFilters />
-
-
         </div>
-
-
-    )
-
+    );
 }
 export default TodoList;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,33 +1,20 @@
 import React from "react";
+
 import TodoInput from './TodoInput.js';
 import TodoList from "./TodoList.js";
 import Todo from "./Todo.js";
 
-
-function App() {
-	return (
+const Home = () => {
+	return(
 		<div className="bg-gray-900 min-h-screen h-full text-gray-100 flex items-center justify-center py-20 px-5">
 			<div className="container flex flex-col max-w-xl">
-				<h1> 
-					Todo List
-				</h1>
+				<h1 className="text-3xl font-bold underline">Todo List</h1>
 				<TodoInput />
-				<TodoList>
-					
-					<Todo />
-					<Todo />
-					<Todo />
-					<Todo />
-					<Todo />
-
-				</TodoList>
-
+				<TodoList />
 			</div>
-
 		</div>
-	)
+	);
+};
 
 
-
-}
-export default App;
+export default Home;
