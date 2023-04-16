43be71505e4a40f756b979da02e7d28f31df89f5
Beverly M <124820505+66006600@users.noreply.github.com>
2023-04-16 16:57:08 +0000

    Revisar antes de entregar

--- src/js/component/App.js
+++ src/js/component/App.js
@@ -1,65 +1,68 @@
 import React, { useState } from 'react';
 import '../../styles/index.css';
 
-
 function App() {
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([
+    { id: 1, value: "Hacer la compra" },
+    { id: 2, value: "Llamar al médico" },
+    { id: 3, value: "Ir al gimnasio" },
+    { id: 4, value: "Estudiar para el examen" },
+  ]);
 
-    const [newItem, setNewItem] = useState("");
-    const [items, setitems] = useState([]);
+  function addItem() {
+    if (!newItem) {
+      alert("Enter an item");
+      return;
+    }
 
-    function addItem() {
-
-        if (!newItem) {
-            alert("Enter an item");
-            return;
-        }
-
-        const item = {
-            id: Math.floor(Math.random() * 1000),
-            value: newItem
-        };
-
-        setitems(oldList => [...oldList, item]);
-        setNewItem("")     
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem
     };
 
-    function deleteItem(id) {
-        const newArray = items.filter(item => item.id !== id);
-        setitems(newArray);
-    };
+    setItems(oldList => [...oldList, item]);
+    setNewItem("")
+  };
 
+  function deleteItem(id) {
+    const newArray = items.filter(item => item.id !== id);
+    setItems(newArray);
+  };
 
-    return (
-        <div className='container'>
+  const count = items.filter(item => !item.completed).length;
 
-            <div className='App'>
-                <h1>Todo List</h1>
+  return (
+    <div className='container'>
+      <div className='App'>
+        <h1>Todo List</h1>
+      </div>
+      <div className='TodoInput'>
+        <input
+          type="text"
+          placeholder="Add an item"
+          value={newItem}
+          onChange={e => setNewItem(e.target.value)}
+        />
+        <button className='ButtonAdd' onClick={() => addItem()}>Add</button>
+      </div>
+      <div>
+        <ul>
+          {items.map(item => {
+            return (
+              <li key={item.id}>{item.value}
+                <button className='delete-boton' onClick={() => deleteItem(item.id)}>X</button>
+              </li>
+            )
+          })}
+        </ul>
+        <div>
+          <p className='contenedor' id='LeftItem'>{count} Left Items</p>
 
-            </div>
-
-            <div className='TodoInput'> <input
-                type="text"
-                placeholder="Add an item"
-                value={newItem}
-                onChange={e => setNewItem(e.target.value)}
-                />
-                <button className='ButtonAdd' onClick={() => addItem()}>Add</button> 
-            </div>
-
-            <div>
-                <ul>
-                    {items.map(item => {
-                        return (
-                            <li key={item.id}>{item.value}
-                                <button className='delete-boton' onClick={() => deleteItem(item.id)}>X</button>
-                            </li>
-                        )
-                    })}
-
-                </ul>
-            </div>
-        </div>        
-    )
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
