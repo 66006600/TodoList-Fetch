a41daf56903af41446845b724c5c59baab91f3e4
Beverly M <124820505+66006600@users.noreply.github.com>
2023-05-04 18:24:20 -0400

    mmmm

--- src/js/component/App.js
+++ src/js/component/App.js
@@ -8,125 +8,129 @@ function App() {
 
   useEffect(() => {
     fetch('https://assets.breatheco.de/apis/fake/todos/user/Beverly',
-        {
-          method: 'GET',
-          headers: { 'Content-Type': 'application/json' },          
+      {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      }
+    )
+      .then(response => {
+        if (response.status == 404) {
+          crearLista()
+        } else {
+          return response.json()
         }
-        )
-        .then(response => response.json())
-        .then(data => {
-          console.log(data)
-          setItems(data)
+      })
+      .then(data => { 
+        console.log(data)
+        setItems(data)
+      })
 
-        
-        })
-
-        .catch (error => {
-          console.log(error);
-        } )
+      .catch(error => {
+        console.log(error);
+      })
 
   }, [])
 
 
-    const crearLista = () => {
-      fetch('https://assets.breatheco.de/apis/fake/todos/user/Beverly',
-        {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify([])
-        }
-      )
-        .then(response => response.json())
-        .then(data => {
-          console.log(data)
-        })
-        .catch(error => console.error(error));
-    }
-
-
-
-
-
-
-    function addItem() {
-      if (!newItem) {
-        alert("DIEGO: Enter an item");
-        return;
-      }
-
-      const item = {
-        value: newItem
-      };
-
-      fetch('https://assets.breatheco.de/apis/fake/todos/user/Beverly', {
+  const crearLista = () => {
+    fetch('https://assets.breatheco.de/apis/fake/todos/user/Beverly',
+      {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(item)
+        body: JSON.stringify([])
+      }
+    )
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
       })
-        .then(response => response.json())
-        .then(data => {
-          setItems(oldList => [...oldList, data.todo]);
-          setCount(oldCount => oldCount + 1);
-          setNewItem("");
-        })
-        .catch(error => console.error(error));
+      .catch(error => console.error(error));
+  }
+
+
+
+
+
+
+  function addItem() {
+    if (!newItem) {
+      alert("DIEGO: Enter an item");
+      return;
+    }
+
+    const item = {
+      value: newItem
     };
 
-    // function deleteItem(id) {
-    //   fetch(`/api/todos/${id}`, {
-    //     method: 'DELETE'
-    //   })
-    //     .then(() => {
-    //       const newArray = items.filter(item => item.id !== id);
-    //       setItems(newArray);
-    //       setCount(oldCount => oldCount - 1);
-    //     })
-    //     .catch(error => console.error(error));
-    // };
-
-    function deleteAll() {
-      fetch('https://assets.breatheco.de/apis/fake/todos/user/Beverly', {
-        method: 'DELETE'
+    fetch('https://assets.breatheco.de/apis/fake/todos/user/Beverly', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(item)
+    })
+      .then(response => response.json())
+      .then(data => {
+        setItems(oldList => [...oldList, data.todo]);
+        setCount(oldCount => oldCount + 1);
+        setNewItem("");
       })
-        .then(() => {
-          setItems([]);
-          setCount(0);
-        })
-        .catch(error => console.error(error));
-    };
+      .catch(error => console.error(error));
+  };
 
-    return (
-      <div className='container'>
-        <div className='App'>
-          <h1>Todo List</h1>
-        </div>
-        <div className='TodoInput'>
-          <input
-            type="text"
-            placeholder="Add an item"
-            value={newItem}
-            onChange={e => setNewItem(e.target.value)}
-          />
-          <button className='ButtonAdd' onClick={() => addItem()}>Add</button>
-        </div>
-        <div>
-          <ul>
-            {items.map((item,index) => {
-              return (
-                <li key={index}>{item.label}
-                  <button className='delete-boton' onClick={() => deleteItem(item.id)}>X</button>
-                </li>
-              )
-            })}
-          </ul>
-          <div className='contenedor'>
-            <p id='LeftItem'>{count} Left Items</p>
-            <div> <button className='ButtonAdd' onClick={() => deleteAll()}>Clear All</button>
-            </div>
+  // function deleteItem(id) {
+  //   fetch(`/api/todos/${id}`, {
+  //     method: 'DELETE'
+  //   })
+  //     .then(() => {
+  //       const newArray = items.filter(item => item.id !== id);
+  //       setItems(newArray);
+  //       setCount(oldCount => oldCount - 1);
+  //     })
+  //     .catch(error => console.error(error));
+  // };
+
+  function deleteAll() {
+    fetch('https://assets.breatheco.de/apis/fake/todos/user/Beverly', {
+      method: 'DELETE'
+    })
+      .then(() => {
+        setItems([]);
+        setCount(0);
+      })
+      .catch(error => console.error(error));
+  };
+
+  return (
+    <div className='container'>
+      <div className='App'>
+        <h1>Todo List</h1>
+      </div>
+      <div className='TodoInput'>
+        <input
+          type="text"
+          placeholder="Add an item"
+          value={newItem}
+          onChange={e => setNewItem(e.target.value)}
+        />
+        <button className='ButtonAdd' onClick={() => addItem()}>Add</button>
+      </div>
+      <div>
+        <ul>
+          {items.map((item, index) => {
+            return (
+              <li key={index}>{item.label}
+                <button className='delete-boton' onClick={() => deleteItem(item.id)}>X</button>
+              </li>
+            )
+          })}
+        </ul>
+        <div className='contenedor'>
+          <p id='LeftItem'>{count} Left Items</p>
+          <div> <button className='ButtonAdd' onClick={() => deleteAll()}>Clear All</button>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
 
 export default App;
